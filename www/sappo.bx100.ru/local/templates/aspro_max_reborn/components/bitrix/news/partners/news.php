fe0b7dc04b3d4e9e2bc5c9ef3ff978710dcf82cc
Одногулов Руслан Хосянович <orh@cds.guru>
2024-10-01 18:29:54 +0300

    files

--- www/sappo.bx100.ru/local/templates/aspro_max_reborn/components/bitrix/news/partners/news.php
+++ www/sappo.bx100.ru/local/templates/aspro_max_reborn/components/bitrix/news/partners/news.php
@@ -56,7 +56,17 @@ $itemsCnt = CMaxCache::CIblockElement_GetList(array("CACHE" => array("TAG" => CM
 	<?//global $arTheme;?>
 	<?// section elements?>
 	<?$sViewElementsTemplate = ($arParams["SECTION_ELEMENTS_TYPE_VIEW"] == "FROM_MODULE" ? $arTheme["PARTNERS_PAGE"]["VALUE"] : $arParams["SECTION_ELEMENTS_TYPE_VIEW"]);?>
-	<?@include_once('page_blocks/'.$sViewElementsTemplate.'.php');?>
+
+    <?php?>
+
+    <form class="brands-search" action="/brands/" method="GET">
+        <input id="tq_brand_search" type="text" placeholder="Поиск по бренду" class="search-field" name="brand" value="">
+        <button type="submit" class="submit-btn">
+            <img src="<?=SITE_TEMPLATE_PATH?>/images/svg/search-btn.svg" alt="Поиск по производителям">
+        </button>
+    </form>
+
+    <?@include_once('page_blocks/'.$sViewElementsTemplate.'.php');?>
 
 	<?if((isset($_SERVER["HTTP_X_REQUESTED_WITH"]) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == "xmlhttprequest") || (strtolower($_REQUEST['ajax']) == 'y'))
 	{
