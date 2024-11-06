18613990825fa3bb840f7eb1fa405a46c5202a5c
Одногулов Руслан Хосянович <orh@cds.guru>
2024-11-06 10:33:33 +0300

    fix

---
+++ www/sappo.bx100.ru/local/components/ahiles3005/seo.meta.tags/templates/toggle/result_modifier.php
@@ -0,0 +1,12 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+
+$newsItems = [];
+
+foreach ($arResult['ITEMS'] as $Item) {
+    if(strstr($Item['REAL_URL'],'brand-is-') !== false){
+        $newsItems[] = $Item;
+    }
+}
+
+$arResult['ITEMS'] = $newsItems;
