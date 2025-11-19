2f23c3851eac83b634c73e7fec0826d7df3f626f
Одногулов Руслан Хосянович <orh@cds.guru>
2025-11-20 02:11:02 +0300

    новая сортировка для страниц скрыть стикеры на главной

--- www/sappo.bx100.ru/local/templates/aspro_max_reborn/components/bitrix/catalog.smart.filter/main/template.php
+++ www/sappo.bx100.ru/local/templates/aspro_max_reborn/components/bitrix/catalog.smart.filter/main/template.php
@@ -1,6 +1,7 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 $this->setFrameMode(true);
 use \Bitrix\Main\Localization\Loc;
+CJSCore::Init(['popup']);
 if($arResult["ITEMS"]){?>
 	<?global $filter_exists;?>
 	<?$filter_exists = "filter_exists";?>
